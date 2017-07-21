2ac602e6595bb5dedc4709c46d0bda9b57aeb793
JonathanHeidy <vashjhk@gmail.com>
2017-07-21 20:02:16 -0300

    Finalização da main

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,105 @@
+import java.util.Random;
+
 /**
- * Created by yellow-umbrella on 17/07/17.
+ * 
+ * 
+ * @author Renan Bennati Dias
+ * @author Jonathan Heidy Kinjo
+ *
+ * Para compilar o trabalho rode "javac Main.java" no root do trabalho
+ * Para rodar o trabalho rode "java Main 1000 200 15 3 0 12 1 6 2 7"
  */
 public class Main {
 
     public static void main(String args[]) {
 
-        System.out.println("Inıcio da simulacao com a substituicao FIFO");
+		/**
+		 * Caso nao haja argumentos suficiente uma mensagem de erro e mostrada
+		 * para o usuario.
+		 */
+		if (args.length < 4) {
+			System.out.println("Quantidade de argumentos faltando.");
+			System.exit(1);
+		}
 
-        int nProc = 3;
-        int nFrames = 6;
+		/**
+		 * Pega o valor do seed.
+		 */
+		int seed = Integer.parseInt(args[0]);
 
-        MainMemory memory = new MainMemory(nFrames);
+		/**
+		 * Pega a quantidade de referencias
+		 */
+		int nref = Integer.parseInt(args[1]);
 
-        MyProcess.nProc = nProc;
-        MyProcess.memory = memory;
+		/**
+		 * Pega quantidade de frames
+		 */
+		int nframes = Integer.parseInt(args[2]);
 
-        MyProcess p1 = new MyProcess(0, 4);
-        MyProcess p2 = new MyProcess(1, 4);
-        MyProcess p3 = new MyProcess(2, 4);
+		/**
+		 * Pega quantidade de processos
+		 */
+		int nproc = Integer.parseInt(args[3]);
 
-        p1.useFrame(0);
-        p1.useFrame(1);
-        p1.useFrame(2);
-        p1.useFrame(3);
 
-        p2.useFrame(0);
-        p2.useFrame(1);
-        p2.useFrame(2);
-        p1.useFrame(1);
-        p2.useFrame(3);
+		MyProcess process[] = new MyProcess[nproc];
+		
+		 MainMemory memory = new MainMemory(nframes);
 
+	     MyProcess.nProc = nproc;
+	     MyProcess.memory = memory;
+
+
+		int f = 0;
+		int argsIndex = 4;
+		do {
+			int proId = Integer.parseInt(args[argsIndex++]);
+			int proSize = Integer.parseInt(args[argsIndex++]);
+
+			process[f++] = new MyProcess(proId, proSize);
+		} while (argsIndex < args.length);
+
+
+		Random rng = new Random(seed);
+
+        System.out.println("Inicio da simulacao com a substituicao FIFO");
+
+        for (int i = 0; i < nref; i++) {
+        	  MyProcess aux = process[rng.nextInt(nproc)];
+        	  aux.useFrame(rng.nextInt(nproc));
+        	
+        }
+        
         memory.printMainMemory();
         System.out.printf("O numero de falhas de pagina foi %d\n", memory.fails);
 
-        System.out.println("P1");
-        p1.printPageTable();
-
-        System.out.println("P2");
-        p2.printPageTable();
-
         System.out.println("Fim da simulacao com a substituicao FIFO");
 
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+        System.out.println("\n\n\n");
 
-        System.out.println("Inıcio da simulacao com a substituicao Segunda chance");
+        System.out.println("Inicio da simulacao com a substituicao Segunda chance");
 
-        SecondMaimMemory second = new SecondMaimMemory(nFrames);
+        SecondMaimMemory second = new SecondMaimMemory(nframes);
 
         MyProcess.memory = second;
+        
+    	f = 0;
+		argsIndex = 4;
+		do {
+			int proId = Integer.parseInt(args[argsIndex++]);
+			int proSize = Integer.parseInt(args[argsIndex++]);
 
-        p1 = new MyProcess(0, 4);
-        p2 = new MyProcess(1, 4);
-        p3 = new MyProcess(2, 4);
+			process[f++] = new MyProcess(proId, proSize);
+		} while (argsIndex < args.length);
 
-        p1.useFrame(0);
-        p1.useFrame(1);
-        p1.useFrame(2);
-        p1.useFrame(3);
-
-        p2.useFrame(0);
-        p2.useFrame(1);
-        p2.useFrame(2);
-        p1.useFrame(1);
-        p2.useFrame(3);
-
+        for (int i = 0; i < nref; i++) {
+      	  MyProcess aux = process[rng.nextInt(nproc)];
+      	  aux.useFrame(rng.nextInt(nproc));
+      	
+      }
         second.printMainMemory();
         System.out.printf("O numero de falhas de pagina foi %d\n", second.fails);
-
-        System.out.println("P1");
-        p1.printPageTable();
-
-        System.out.println("P2");
-        p2.printPageTable();
 
         System.out.println("Fim da simulacao com a substituicao Segunda chance");
 
